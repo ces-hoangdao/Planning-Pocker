8b173698ca1e965e84786dcbc40ef37597edb53b
HoaiAnhTran <hoaianhdn1711@gmail.com>
2023-06-23 09:56:16 +0700

    feat: create form new game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginAsGuest from "./pages/LoginAsGuest"
 import RoomHeader from "./pages/RoomHeader"
+import NewGame from "./pages/NewGame"
 import ROUTES from "./constants/routes"
 import "./index.css"
 import Home from "./pages/Home"
@@ -14,6 +15,7 @@ function App() {
           <Route path={ROUTES.GUEST_PATH} element={<LoginAsGuest />} />
           <Route path={ROUTES.ROOM_PATH} element={<RoomHeader />} />
           <Route path={ROUTES.HOME_PATH} element={<Home />} />
+          <Route path={ROUTES.NEW_GAME_PATH} element={<NewGame />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/LoginAsGuest/index.js
+++ src/pages/LoginAsGuest/index.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react"
+import guestLogin from "../../api/services/authService"
 import "./LoginAsGuest.css"
 
 function LoginAsGuest() {
   const [guestName, setGuestName] = useState("")
   const [showError, setShowError] = useState(false)
 
-  const handleClick = () => {
-    setShowError(guestName.trim() === "")
-  }
-
   const handleInputChange = (event) => {
     setGuestName(event.target.value)
     setShowError(false)
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (guestName.trim() === "") {
+      setShowError(true)
+    } else {
+      const res = await guestLogin(guestName)
+      setGuestName(res.data.name)
+    }
+  }
+
   return (
     <div className="login-as-guest">
       <h3 className="title">Choose your display name</h3>
-      <form action="" className="form-login-as-guest">
+      <form className="form-login-as-guest" onSubmit={handleSubmit}>
         <input
           type="text"
           required
@@ -33,9 +40,9 @@ function LoginAsGuest() {
           </div>
         )}
         <button
-          type="button"
+          type="submit"
           className="button button--primary button-continue"
-          onClick={handleClick}
+          onClick={handleSubmit}
         >
           Continue to game
         </button>
