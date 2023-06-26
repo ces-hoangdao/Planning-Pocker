52693d0568d8fe9aabb9cc2c460258b300617c89
HoaiAnhTran <hoaianhdn1711@gmail.com>
2023-06-26 11:47:26 +0700

    feat: create room header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginAsGuest from './pages/LoginAsGuest';
-import ROUTES from './constants/routes';
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react"
+import LoginAsGuest from "./pages/LoginAsGuest"
+import RoomHeader from "./pages/RoomHeader"
+import ROUTES from "./constants/routes"
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={ROUTES.GUEST_PATH} element={ <LoginAsGuest />} />
+          <Route path={ROUTES.GUEST_PATH} element={<LoginAsGuest />} />
+          <Route path={ROUTES.ROOM_PATH} element={<RoomHeader />} />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,8 @@
 const ROUTES = {
-    HOME_PATH: '/',
-    GUEST_PATH: 'guest'
-};  
+  HOME_PATH: "/",
+  GUEST_PATH: "guest",
+  NEW_GAME_PATH: "new-game",
+  ROOM_PATH: "room/:id",
+}
 
-export default ROUTES;
-
+export default ROUTES
