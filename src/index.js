c68389022522d47bdbb7401a597e6fc74094a9c2
Azanamin <minhnhat912002@gmail.com>
2023-06-30 17:19:32 +0700

    feat: implement socket.io on client

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap/dist/css/bootstrap.css"
+import SocketContextProvider from "./context/SocketContext"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <SocketContextProvider>
+      <App />
+    </SocketContextProvider>
   </React.StrictMode>
 )
