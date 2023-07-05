089df7c99f69d25f640f473822e263df0c67133b
Azanamin <minhnhat912002@gmail.com>
2023-07-05 11:30:32 +0700

    refactor: refactor UserContext and google redirecting

--- src/pages/Login/Google/index.js
+++ src/pages/Login/Google/index.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react"
+import React, { useContext, useEffect } from "react"
 import { toast } from "react-toastify"
 import { useParams, useNavigate } from "react-router-dom"
 import { ROUTES } from "../../../constants/routes"
+import { UserContext } from "../../../context/userContext"
+import getUserById from "../../../api/services/userService"
 
 function GoogleLogin() {
+  const { setUser } = useContext(UserContext)
   const { id } = useParams()
+
   const navigate = useNavigate()
+
   useEffect(() => {
     localStorage.setItem("userId", id)
+    const getUser = async () => {
+      if (id) {
+        const res = await getUserById(id)
+        setUser(res.data)
+      }
+    }
+    getUser()
     navigate(ROUTES.NEW_GAME_PATH)
     toast.success("Login success!")
   }, [])
+
   return <div> </div>
 }
 
